7bd3f60b1f6f4af1a036e4611fb59baded5385f4
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-10-15 05:11:25 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-
 import "./globals.css";
 import Header from "@/components/Header";
 import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = { title: "FIxy | Dashboard Comercio" };
 
@@ -10,7 +10,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="es">
       <body>
         <Header />
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</main>
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          {children}
+          <Analytics />
+        </main>
       </body>
     </html>
   );
